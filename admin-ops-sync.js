657e280bdaf1d45bdceca1813ae527b31590126b
officiallightningdispatch <officiallightningdispatch@gmail.com>
2026-09-16 23:16:00 -0500

    Point admin tracker to latest Fall Festival needs file

--- admin-ops-sync.js
+++ admin-ops-sync.js
@@ -4,7 +4,7 @@
 
   const SESSION_KEY='voigt-pta-board-session';
   const VOLUNTEER_TARGET=55;
-  const NEEDS_URL='https://drive.google.com/file/d/1QV23UzTNOMB_Mf7mDvLTKXTA3NJD2_ph/view?usp=drivesdk';
+  const NEEDS_URL='https://drive.google.com/file/d/19eDjjtYKku7L0FKOrjsIqbmy-Yf1G2AP/view?usp=drivesdk';
   const MERCH_URL='https://docs.google.com/spreadsheets/d/18svZh4ynkP8a9A-98Zsir4l4wipvoCPuOZXyWiSSPzw/edit#gid=739301024';
   const PRINTIFY_URL='https://printify.com/pop-up-store/';
 
